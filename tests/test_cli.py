29ed3508fbf4ba4a165eb4a765c9e6d8e1603a39
Dominik Walk <dominik.walk@mail.de>
2021-01-07 13:56:10 +0100

    implemented cli failure test

---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+from _pytest.capture import capsys
+import pytest
+import json
+
+import importtime_output_wrapper
+
+
+def test_cli_empty_module():
+    with pytest.raises(SystemExit) as e:
+        importtime_output_wrapper.main([])
+        assert e.value == 2
